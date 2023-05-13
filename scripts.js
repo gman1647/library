1a3f90b836c533fa8e4ba12eae7a87cf03402610
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2023-05-13 16:20:55 -0400

    Test

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return title + ' by ' + author + ', ' + pages + ', ' + read;
+  };
+}
+
+/* console.log(theHobbit.info()); */
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.sayName = function () {
+    console.log(name);
+  };
+}
+
+const player1 = new Player('steve', 'X');
+const player2 = new Player('also steve', 'O');
+
+const high = 'hi';
