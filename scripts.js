1482546840335b7343957d58dc74aa109bbe0957
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2023-05-14 23:38:23 -0400

    Style Page Header

--- scripts.js
+++ scripts.js
@@ -21,11 +21,19 @@ addBookToLibrary(book1);
 console.log(myLibrary);
 
 const newBookForm = document.getElementById('book-form');
-let newBookButton = document.getElementById('add-book');
 
-newBookButton.addEventListener('click', () => {
+const newBookButton = document.getElementById('add-book-header-button');
+
+/* newBookButton.addEventListener('click', () => {
   console.log(newBookForm.style.display);
   newBookForm.style.display === 'none' || newBookForm.style.display === ''
     ? (newBookForm.style.display = 'block')
+    : (newBookForm.style.display = 'none');
+}); */
+
+newBookButton.addEventListener('click', () => {
+  style = window.getComputedStyle(newBookForm);
+  style.getPropertyValue('display') === 'none'
+    ? (newBookForm.style.display = 'grid')
     : (newBookForm.style.display = 'none');
 });
