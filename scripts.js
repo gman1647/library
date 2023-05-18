1efdfb7ae8fe186ac9dec06f47f514a279a710ff
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2023-05-17 23:53:18 -0400

    Add Function to Add New Book Card

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 let myLibrary = [];
+const cardContainer = document.getElementById('card_container');
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return title + ' by ' + author + ', ' + pages + ', ' + read;
-  };
 }
 
 function addBookToLibrary(book) {
@@ -53,15 +51,50 @@ submitForm.addEventListener('click', () => {
 });
 
 let getNewBookInfo = () => {
-  let author = document.getElementById('new-author');
-  let title = document.getElementById('new-title');
-  let pages = document.getElementById('new-pages');
-  let read = document.getElementById('new-read');
-  let object = {};
+  let author = document.getElementById('new-author').value;
+  let title = document.getElementById('new-title').value;
+  let pages = document.getElementById('new-pages').value;
+  let read = checkRead();
+  /*   let read = document.getElementById('new-read');
+  read.checked == true ? (read = true) : (read = false); */
+  /*   let object = {};
   object.author = author.value;
   object.title = title.value;
   object.pages = pages.value;
-  read.checked == true ? (object.read = true) : (object.read = false);
-  myLibrary.push(object);
-  console.log(myLibrary);
+  read.checked == true ? (object.read = true) : (object.read = false); */
+  const book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
 };
+
+let checkRead = () => {
+  let read = document.getElementById('new-read');
+  read.checked == true ? (read = true) : (read = false);
+  return read;
+};
+
+let makeBookCard = (title, author, pages, read) => {
+  const mainDiv = document.createElement('div');
+
+  const tit = document.createElement('h3');
+  tit.classList.add('book-title');
+  tit.textContent = title;
+
+  const auth = document.createElement('p');
+  auth.classList.add('book-author');
+  auth.textContent = author;
+
+  const pag = document.createElement('p');
+  pag.classList.add('book-pages');
+  pag.textContent = pages;
+
+  mainDiv.appendChild(tit);
+  mainDiv.appendChild(auth);
+  mainDiv.appendChild(pag);
+
+  mainDiv.classList.add('card');
+  cardContainer.insertBefore(mainDiv, newBookForm);
+};
+
+makeBookCard('Les Miserable', 'Victor Hugo', 1234, true);
+
+makeBookCard('Black Forrest Rangers', 'G. Bearrymore', 548, false);
